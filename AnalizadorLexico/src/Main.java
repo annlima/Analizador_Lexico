79764d17bdfe1631033e30bad9b6f647169ad9e5
Andy <alimablanca@gmail.com>
2023-11-19 17:24:17 -0600

    Cambios

--- AnalizadorLexico/src/Main.java
+++ AnalizadorLexico/src/Main.java
@@ -1,12 +1,15 @@
-import java.lang.reflect.Array;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    /**
+     * Método principal para iniciar el análisis léxico.
+     * @param args Argumentos de línea de comandos (no utilizados).
+     */
     public static void main(String[] args){
+        // Entrada de ejemplo para el análisis léxico
         String input = "while a < b do\n" +
                 "if f == var then\n" +
                 "b = c2 + 99\n" +
@@ -15,42 +18,69 @@ public class Main {
                 "endif\n" +
                 "endwhile";
         System.out.println("Input: " + input);
+
+        // Proceso de análisis léxico y obtención de tokens
         ArrayList<Token> tokens = lex(input);
-        for (Token token: tokens){
+        for (Token token : tokens){
             System.out.println(" Value: " + token.getValue() + " Type: " + token.getType() + " Lexeme: " + token.getLexeme());
         }
     }
 
-    private static ArrayList<Token> lex(String input){
-        final ArrayList<Token> tokens = new ArrayList<>(); //Arraylist that will store the tokens found
-        final StringTokenizer st = new StringTokenizer(input); //divides phrases and words
+    /**
+     * Realiza el análisis léxico de la cadena de entrada y devuelve una lista de tokens.
+     * @param input Cadena de entrada para analizar.
+     * @return ArrayList de tokens encontrados en la entrada.
+     */
+    private static ArrayList<Token> lex(String input) {
+        final ArrayList<Token> tokens = new ArrayList<>();
+        final StringTokenizer st = new StringTokenizer(input);
 
-        while (st.hasMoreTokens()){
+        while (st.hasMoreTokens()) {
             String word = st.nextToken();
             boolean flag = false;
 
-            for (Token.Type tokenType: Token.Type.values()){
+            // Procesamiento de cada tipo de token excepto VARIABLE
+            for (Token.Type tokenType : Token.Type.values()) {
+                if (tokenType == Token.Type.VARIABLE) {
+                    continue;
+                }
 
                 Pattern pattern = Pattern.compile(tokenType.pattern);
                 Matcher search = pattern.matcher(word);
-                if (search.find()){
+                if (search.find()) {
                     Token token = new Token();
                     token.setType(tokenType);
                     token.setValue(word);
                     matchLexeme(word, token);
                     tokens.add(token);
                     flag = true;
-                    //System.out.println(" Value: " + token.getValue() + " Type: " + token.getType() + " Lexeme: " + token.getLexeme());
+                    break;
                 }
             }
-            if (!flag){
-                throw new RuntimeException("Invalid token: " + word);
 
+            // Verificación final para el tipo VARIABLE
+            if (!flag) {
+                Pattern pattern = Pattern.compile(Token.Type.VARIABLE.pattern);
+                Matcher search = pattern.matcher(word);
+                if (search.find()) {
+                    Token token = new Token();
+                    token.setType(Token.Type.VARIABLE);
+                    token.setValue(word);
+                    tokens.add(token);
+                } else {
+                    // Si no se encuentra ningún tipo de token válido, se lanza una excepción
+                    throw new RuntimeException("Invalid token: " + word);
+                }
             }
         }
         return tokens;
     }
 
+    /**
+     * Asigna un lexema al token basado en su valor.
+     * @param word La palabra actual para la que se está creando el token.
+     * @param token El token al que se le asignará el lexema.
+     */
     private static void matchLexeme(String word, Token token){
         for (Token.Lexeme lexeme : Token.Lexeme.values()) {
             if (word.equals(lexeme.lexeme)) {
@@ -58,13 +88,13 @@ public class Main {
                 break;
             }
         }
-        // If no lexeme is matched, the token's lexeme remains null
     }
+
+    // Los siguientes métodos podrían ser útiles para futuras expansiones o verificaciones adicionales
     private static boolean isConditionalOperand(String word) {
         return word.matches("\\b(if|then|endif|else|while|do|endwhile)\\b");
     }
     private static boolean isComparisonOperand(String word) {
         return word.matches("==|<|<=|>|>=|<>");
     }
-
 }
--- AnalizadorLexico/src/Token.java
+++ AnalizadorLexico/src/Token.java
@@ -1,45 +1,83 @@
+/**
+ * La clase Token representa un token individual identificado durante el análisis léxico.
+ * Un token contiene información sobre su valor, tipo y lexema correspondiente.
+ */
 public class Token {
-    private String value;
-    private Type type;
-    private Lexeme lexeme;
+    private String value; // Valor del token (cadena exacta encontrada en el input)
+    private Type type;    // Tipo del token (enum Type)
+    private Lexeme lexeme; // Lexema asociado al token (enum Lexeme)
 
+    /**
+     * Obtiene el tipo del token.
+     * @return El tipo del token.
+     */
     public Type getType(){
         return type;
     }
+
+    /**
+     * Establece el tipo del token.
+     * @param type El tipo del token a establecer.
+     */
     public void setType(Type type){
         this.type = type;
     }
+
+    /**
+     * Obtiene el valor del token.
+     * @return El valor del token.
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Establece el valor del token.
+     * @param value El valor del token a establecer.
+     */
     public void setValue(String value) {
         this.value = value;
     }
+
+    /**
+     * Establece el lexema del token.
+     * @param lexeme El lexema a establecer para este token.
+     */
     public void setLexeme(Lexeme lexeme){
         this.lexeme = lexeme;
     }
+
+    /**
+     * Obtiene el lexema del token.
+     * @return El lexema del token.
+     */
     public Lexeme getLexeme(){
         return lexeme;
     }
 
+    /**
+     * Enumeración de los diferentes tipos de tokens que pueden ser identificados.
+     */
     enum Type{
-        NUMBER("\\b[0-9]+\\b"),
-        MATH_OPERAND("[+\\-*/]"),
-        COMPARISON_OPERAND("==|<|<=|>|>=|<>"),
-        ASSIGNATION_OPERAND("(?<!=)="),
-        CONDITIONAL_OPERAND("\\b(if|then|endif|else)\\b"),
-        BOOLEAN_VALUE("\\b(true|false)\\b"),
-        LOOP("\\b(while|do|endwhile)\\b"),
-        VARIABLE("(?!\\b(if|then|endif|else|while|do|endwhile|true|false)\\b)[a-zA-Z_][a-zA-Z0-9_]*");
-        //PROGRAM NOT RECOGNIZING BETWEEN A VARIABLE AND A CONDITIONAL OPERAND
-        public final String pattern;
+        NUMBER("\\b[0-9]+\\b"),                 // Representa un número
+        MATH_OPERAND("[+\\-*/]"),               // Representa un operador matemático
+        COMPARISON_OPERAND("==|<|<=|>|>=|<>"),  // Representa un operador de comparación
+        ASSIGNATION_OPERAND("(?<!=)="),         // Representa un operador de asignación
+        CONDITIONAL_OPERAND("\\b(if|then|endif|else)\\b"), // Representa un operando condicional
+        BOOLEAN_VALUE("\\b(true|false)\\b"),    // Representa un valor booleano
+        LOOP("\\b(while|do|endwhile)\\b"),      // Representa un operador de bucle
+        VARIABLE("(?!\\b(if|then|endif|else|while|do|endwhile|true|false)\\b)[a-zA-Z_][a-zA-Z0-9_]*"); // Representa una variable
+
+        public final String pattern; // Patrón de expresión regular para identificar el tipo de token
 
         Type(String s) {
             this.pattern = s;
         }
     }
 
+    /**
+     * Enumeración de los diferentes lexemas que pueden ser asociados a los tokens.
+     */
     enum Lexeme{
         WHILE ("while"),
         DO ("do"),
@@ -58,7 +96,7 @@ public class Token {
         LESS_EQUAL_THAN("<="),
         GREATER_THAN(">"),
         GRATER_EQUAL_THAN(">=");
-        public final String lexeme;
+        public final String lexeme; // Representación de cadena del lexema
 
         Lexeme(String l) {
             this.lexeme = l;
